47d0ea9b55d2fc94f7eaa6715c120a78ef1261fd
BRicsi <barnusrichi@gmail.com>
2024-11-15 13:14:10 +0100

    Add project files.

---
+++ Connect.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basket_team
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string DataBase;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+        
+        public Connect()
+        {
+        Host = "localhost";
+        DataBase = "Team";
+        User = "root";
+        Password = "";
+
+        ConnectionString = "SERVER=" + Host + ";DATABASE=" + DataBase + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+        Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
